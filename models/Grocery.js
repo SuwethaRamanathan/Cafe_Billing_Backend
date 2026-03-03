8798e69f1ad9c76bb2110e4b0528bd39ba5eb4cc
Suwetha_Ramanathan <swetharammu007@gmail.com>
2026-03-03 11:50:13 +0530

    Fix new raw material

--- models/Grocery.js
+++ models/Grocery.js
@@ -5,10 +5,10 @@ const grocerySchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  unit: {
-    type: String, 
-    required: true
-  },
+  // unit: {
+  //   type: String, 
+  //   required: true
+  // },
   quantity: {
     type: Number,
     required: true,
