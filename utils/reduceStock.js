08fe4ae1e9cdd18b801d4a6c475ebf47ef8fc056
Suwetha_Ramanathan <swetharammu007@gmail.com>
2026-03-12 20:05:00 +0530

    Fix qty reduce

--- utils/reduceStock.js
+++ utils/reduceStock.js
@@ -43,19 +43,21 @@
 
 // }
 
+
+
 import Grocery from "../models/Grocery.js";
 
-export async function reduceStock(menuItem, orderedQty){
+export async function reduceStock(menuItem, orderedQty) {
 
   const ingredientTotals = {};
 
-  for(const ing of menuItem.recipe){
+  for (const ing of menuItem.recipe) {
 
     const gid = ing.grocery.toString();
 
     const requiredQty = ing.qty * orderedQty;
 
-    if(!ingredientTotals[gid]){
+    if (!ingredientTotals[gid]) {
       ingredientTotals[gid] = 0;
     }
 
@@ -63,26 +65,76 @@ export async function reduceStock(menuItem, orderedQty){
 
   }
 
-  for(const gid in ingredientTotals){
+  for (const gid in ingredientTotals) {
 
     const grocery = await Grocery.findById(gid);
 
-    if(grocery.quantity < ingredientTotals[gid]){
+    if (!grocery) {
+      throw new Error("Recipe contains missing grocery item");
+    }
+
+    if (grocery.quantity < ingredientTotals[gid]) {
       throw new Error(`Insufficient raw material`);
     }
 
   }
 
-  for(const gid in ingredientTotals){
+  for (const gid in ingredientTotals) {
 
     await Grocery.findByIdAndUpdate(
       gid,
       {
-        $inc:{ quantity:-ingredientTotals[gid] },
-        lastStockUpdatedDate:new Date()
+        $inc: { quantity: -ingredientTotals[gid] },
+        lastStockUpdatedDate: new Date()
       }
     );
 
   }
 
 }
+
+
+
+// import Grocery from "../models/Grocery.js";
+
+// export async function reduceStock(menuItem, orderedQty){
+
+//   const ingredientTotals = {};
+
+//   for(const ing of menuItem.recipe){
+
+//     const gid = ing.grocery.toString();
+
+//     const requiredQty = ing.qty * orderedQty;
+
+//     if(!ingredientTotals[gid]){
+//       ingredientTotals[gid] = 0;
+//     }
+
+//     ingredientTotals[gid] += requiredQty;
+
+//   }
+
+//   for(const gid in ingredientTotals){
+
+//     const grocery = await Grocery.findById(gid);
+
+//     if(grocery.quantity < ingredientTotals[gid]){
+//       throw new Error(`Insufficient raw material`);
+//     }
+
+//   }
+
+//   for(const gid in ingredientTotals){
+
+//     await Grocery.findByIdAndUpdate(
+//       gid,
+//       {
+//         $inc:{ quantity:-ingredientTotals[gid] },
+//         lastStockUpdatedDate:new Date()
+//       }
+//     );
+
+//   }
+
+// }
