a8f6aa85ed10b2eb38bcb957816d2bfc10950598
Suwetha_Ramanathan <swetharammu007@gmail.com>
2026-03-12 16:01:46 +0530

    Fix new sadmin

--- routes/auth.js
+++ routes/auth.js
@@ -6,22 +6,6 @@ import User from "../models/User.js";
 
 const router = express.Router();
 
-// router.post("/register", async (req, res) => {
-
-//   const { name, email, password, role } = req.body;
-
-//   const hash = await bcrypt.hash(password, 10);
-
-//   const user = await User.create({
-//     name,
-//     email,
-//     password: hash,
-//     role
-//   });
-
-//   res.json({ success: true });
-
-// });
 
 router.post("/register", async (req, res) => {
   try {
