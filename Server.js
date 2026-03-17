e8e8706c3c6c09aef78b37d821cf1b5062baf55b
Suwetha_Ramanathan <swetharammu007@gmail.com>
2026-03-17 10:58:06 +0530

    Fix mail2

--- Server.js
+++ Server.js
@@ -44,7 +44,7 @@ app.use("/api/units", unitRoutes);
 app.use("/api/settings", settingsRoutes);
 
 app.use("/api/superadmin", superAdminRoutes);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
